481ddabfee5d923609d6fdceaf9877b7dece3465
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-04-10 16:08:24 -0500

    Inital commit

---
+++ src/main/java/com/entrylevelcoder/entrylevelcoder/EntrylevelcoderApplication.java
@@ -0,0 +1,13 @@
+package com.entrylevelcoder.entrylevelcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrylevelcoderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrylevelcoderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entrylevelcoder/entrylevelcoder/EntrylevelcoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrylevelcoder.entrylevelcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrylevelcoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
