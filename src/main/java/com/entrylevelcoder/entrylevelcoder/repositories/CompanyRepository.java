c0ec5f512647c22a479948ed651236b89435dd16
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-04-11 10:06:53 -0500

    Create company repository

---
+++ src/main/java/com/entrylevelcoder/entrylevelcoder/repositories/CompanyRepository.java
@@ -0,0 +1,7 @@
+package com.entrylevelcoder.entrylevelcoder.repositories;
+
+import com.entrylevelcoder.entrylevelcoder.models.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
