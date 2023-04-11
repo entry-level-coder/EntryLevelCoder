7494aa0fd076a5ea38b3a1a85adabdb83f63f129
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-04-11 11:56:09 -0500

    post-crud almost done

--- src/main/java/com/entrylevelcoder/entrylevelcoder/repositories/PostRepository.java
+++ src/main/java/com/entrylevelcoder/entrylevelcoder/repositories/PostRepository.java
@@ -1,5 +1,16 @@
 package com.entrylevelcoder.entrylevelcoder.repositories;
 
+import com.entrylevelcoder.entrylevelcoder.models.Post;
+
+import java.util.Optional;
+
 public interface PostRepository {
+
+    Optional<Post> findById(Long id);
+
+    Post findAll();
+
+    Post deleteById(long id);
+
 
 }
