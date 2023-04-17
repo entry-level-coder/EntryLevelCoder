06f3b41d22ffbe9b878adf639752a46fa725a688
Johnny Wells <johnnyw01github@gmail.com>
2023-04-17 11:26:50 -0500

    Removed keys.js

--- src/main/resources/static/js/keys.js
+++
@@ -1,8 +0,0 @@
-// const appId = 'f5c81ba0';
-// const appKey = 'e98078033fa9f579bd2fda7e9d301718';
-// const apiUrl = 'https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=f5c81ba0&app_key=e98078033fa9f579bd2fda7e9d301718&results_per_page=50&what=software%20developer%20&what_exclude=Senior%20&title_only=software&max_days_old=90&category=it-jobs';
-
-const keys = {
-    adzuna : 'https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=f5c81ba0&app_key=e98078033fa9f579bd2fda7e9d301718%09&results_per_page=100&what=software%20web%20developer%20&what_exclude=senior%20mid-level&max_days_old=90&sort_by=date'
-}
-//
