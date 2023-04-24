db66042f602015fef81261521aba70bbec1bb092
Johnny Wells <johnnyw01github@gmail.com>
2023-04-24 10:36:13 -0500

    Refactoring keys file.

---
+++ src/main/java/com/entrylevelcoder/entrylevelcoder/controllers/ApiController.java
@@ -0,0 +1,21 @@
+package com.entrylevelcoder.entrylevelcoder.controllers;
+
+import com.entrylevelcoder.entrylevelcoder.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApiController {
+
+    @Value("${adzuna_key}")
+    private String adzunaKey;
+
+
+    @GetMapping("/posts")
+    public String returnJobs(Model model){
+        model.addAttribute("adzuna_key", adzunaKey);
+        return "browseJobs";
+    }
+}
