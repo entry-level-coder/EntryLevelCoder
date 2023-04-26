865ceff899a75f9a276e56915e87e8e610a77672
aharownjackson <aharownmjackson@gmail.com>
2023-04-26 14:19:04 -0500

    testing adzuna java workaround

---
+++ src/main/java/com/entrylevelcoder/entrylevelcoder/controllers/AdzunaController.java
@@ -0,0 +1,21 @@
+package com.entrylevelcoder.entrylevelcoder.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AdzunaController {
+    private AdzunaSvc adzunaSvc;
+
+    public AdzunaController(AdzunaSvc adzunaSvc) {
+        this.adzunaSvc = adzunaSvc;
+    }
+
+    @GetMapping("/adzuna")
+    @ResponseBody
+    public String returnJobs() {
+        return adzunaSvc.returnJSON();
+    }
+
+}
