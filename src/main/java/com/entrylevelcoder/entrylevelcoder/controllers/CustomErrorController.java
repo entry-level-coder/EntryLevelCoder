122632accb559b076f53fd64d55820f8d57a6c45
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-04-27 15:49:49 -0500

    added controller and a required dependency to pom.xml

---
+++ src/main/java/com/entrylevelcoder/entrylevelcoder/controllers/CustomErrorController.java
@@ -0,0 +1,29 @@
+package com.entrylevelcoder.entrylevelcoder.controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if(statusCode == HttpStatus.BAD_REQUEST.value()) {
+                return "error/400";
+            } else if(statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "error/403";
+            } else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error/500";
+            }
+        }
+        return "error"; // Fallback to a default error page if needed
+    }
+}
