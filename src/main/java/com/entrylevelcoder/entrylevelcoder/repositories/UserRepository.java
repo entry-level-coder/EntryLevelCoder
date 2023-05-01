6760865339eec64a8f17e136140d15ff6b61c814
Michael-Angel Cevallos <michael.a.cevallos@gmail.com>
2023-05-01 14:21:40 -0500

    you can now changed hashed password, and 'are you sure?' prompt works correctley now'

--- src/main/java/com/entrylevelcoder/entrylevelcoder/repositories/UserRepository.java
+++ src/main/java/com/entrylevelcoder/entrylevelcoder/repositories/UserRepository.java
@@ -4,16 +4,12 @@ import com.entrylevelcoder.entrylevelcoder.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-//    static Optional<User> findById(int userId) {
-//
-//    }
-//    static boolean existsById(int userId) {
-//
-//    }
-//
-    User  deleteById(long id);
-//
+
+    void deleteById(long id);
+
     User findByUsername(String username);
 
     User findById(long id);
+
+
 }
